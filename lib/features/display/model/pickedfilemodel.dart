e567fbc1f645cdbff523ec93acf4c5cf7bd3badb
master <master@git.com>
2024-09-14 10:44:59 +0100

    Works

--- lib/features/display/model/pickedfilemodel.dart
+++ lib/features/display/model/pickedfilemodel.dart
@@ -5,61 +5,57 @@ import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
 import 'package:openpdf/features/display/views/pdf_view.dart';
 import 'package:file_picker/file_picker.dart';
 
-class PickedFileModel {
-  final String fileName;
-  final String filePath;
-  PickedFileModel({
-    required this.fileName,
-    required this.filePath,
-  });
 // Handles picking and pushing the file to a view page
-  Future<void> openFile(
-      BuildContext context, FilePickerResult? fileResult) async {
-    if (context.mounted) {
-      PermissionStatus permStatus =
-          await Permission.manageExternalStorage.request();
-      if (permStatus.isGranted) {
-        if (fileResult != null && fileName.endsWith('.pdf')) {
-          Navigator.of(context).push(
-            MaterialPageRoute(
-              builder: (context) => PdfView(
-                pdfWidget: SfPdfViewer.file(File(filePath)),
-                appBarTitle: fileName.split('.')[0],
-              ),
-            ),
-          );
-        } else if (!fileName.endsWith('.pdf')) {
-          if (context.mounted) {
-            ScaffoldMessenger.of(context).showSnackBar(
-              SnackBar(
-                content: Text('Unable to open $fileName'),
-                behavior: SnackBarBehavior.floating,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
-              ),
-            );
-          }
-        }
-      } else if (permStatus.isDenied) {
-        ScaffoldMessenger.of(context).showSnackBar(
-            const SnackBar(content: Text('Storage access is denied.')));
-
-        await Permission.storage.request();
-      } else if (permStatus.isPermanentlyDenied) {
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(
-            content: const Text(
-                'Storage access is permanently denied. Go to settings?'),
-            action: SnackBarAction(
-              label: 'Settings',
-              onPressed: () {
-                openAppSettings();
-              },
+Future<void> openFile(BuildContext context) async {
+  FilePickerResult? fileResult =
+      await FilePicker.platform.pickFiles(allowMultiple: false);
+  if (context.mounted) {
+    PermissionStatus permStatus = await Permission.storage.request();
+    if (permStatus.isGranted) {
+      if (fileResult != null && fileResult.files.single.name.endsWith('.pdf')) {
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (context) => PdfView(
+              pdfWidget: SfPdfViewer.file(File(fileResult.files.single.path!)),
+              appBarTitle: fileResult.files.single.name.split('.')[0],
             ),
           ),
         );
+      } else if (!fileResult!.files.single.name.endsWith('.pdf')) {
+        if (context.mounted) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Text('Unable to open ${fileResult.files.single.name}'),
+              behavior: SnackBarBehavior.floating,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+            ),
+          );
+        }
       }
+    } else if (permStatus.isDenied) {
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: const Text('Storage access is denied.'),
+        behavior: SnackBarBehavior.floating,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+      ));
+      await Permission.storage.request();
+    } else if (permStatus.isPermanentlyDenied) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: const Text(
+              'Storage access is permanently denied. Go to settings?'),
+          action: SnackBarAction(
+            label: 'Settings',
+            onPressed: () {
+              openAppSettings();
+            },
+          ),
+        ),
+      );
     }
   }
 }
--- lib/features/display/views/home_view.dart
+++ lib/features/display/views/home_view.dart
@@ -1,6 +1,4 @@
 import 'dart:io';
-
-import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:openpdf/features/display/model/pickedfilemodel.dart';
 
@@ -23,13 +21,8 @@ class _HomeViewState extends State<HomeView> {
         title: const Text('OpenPDF'),
         actions: [
           IconButton(
-            onPressed: () async {
-              FilePickerResult? fileResult =
-                  await FilePicker.platform.pickFiles();
-              PickedFileModel(
-                fileName: fileResult!.files.single.name,
-                filePath: fileResult.files.single.path!,
-              ).openFile(context, fileResult);
+            onPressed: () {
+              openFile(context);
             },
             icon: const Icon(Icons.file_open_rounded),
           )
