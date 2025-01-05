88558fab398986398a9048f39fb77a9cbb3ba322
PubSec <pubsecs@proton.me>
2025-01-05 10:10:23 +0200

    UI fix

--- lib/model/pdf_model.dart
+++ lib/model/pdf_model.dart
@@ -1,9 +1,9 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
 class PdfFileModel {
   String fileName;
   String filePath;
-  Icon fileIcon = const Icon(Icons.picture_as_pdf);
+  Icon fileIcon = const Icon(CupertinoIcons.doc_circle);
   // int exitPageNumber = 0;
   PdfFileModel({required this.fileName, required this.filePath});
 }
