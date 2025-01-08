61ce18b9eadc8fbe2dacd204a11006a44c583aaf
PubSec <pubsecs@proton.me>
2025-01-08 07:52:01 +0200

    intent handler not finished

--- lib/intent_handler/intent_handler.dart
+++ lib/intent_handler/intent_handler.dart
@@ -1,5 +1,50 @@
+// import 'package:app_links/app_links.dart';
+// import 'package:flutter/material.dart';
+// import 'package:openpdf/model/pdf_model.dart';
+// import 'package:openpdf/views/pdf_view.dart';
+
+// class IntentHandler extends StatefulWidget {
+//   const IntentHandler({super.key});
+
+//   @override
+//   State<IntentHandler> createState() => _IntentHandlerState();
+// }
+
+// class _IntentHandlerState extends State<IntentHandler> {
+//   final appLinks = AppLinks();
+
+//   @override
+//   Widget build(BuildContext context) {
+//     appLinks.uriLinkStream.listen(
+//       (Uri uri) {
+//         print("*****$uri*****");
+//         uri.toString().split('//');
+//         print("*****$uri*****");
+//         final file = PdfFileModel(
+//             fileName: "${uri.toString().length}", filePath: uri.toString());
+//         if (mounted) {
+//           Navigator.push(
+//             context,
+//             MaterialPageRoute(
+//               builder: (context) => PdfView(pdfFile: file),
+//             ),
+//           );
+//         }
+//       },
+//       cancelOnError: true,
+//     );
+
+//     return const Scaffold(
+//       body: Center(child: Text('Listening for PDF links...')),
+//     );
+//   }
+// }
+
 import 'dart:async';
+import 'package:app_links/app_links.dart';
 import 'package:flutter/material.dart';
+import 'package:openpdf/model/pdf_model.dart';
+import 'package:openpdf/views/pdf_view.dart';
 
 class IntentHandler extends StatefulWidget {
   const IntentHandler({super.key});
@@ -9,24 +54,66 @@ class IntentHandler extends StatefulWidget {
 }
 
 class _IntentHandlerState extends State<IntentHandler> {
-  // ignore: unused_field
-  StreamSubscription? _sub;
+  StreamSubscription<Uri>? _uriLinkSubscription;
+  final AppLinks appLinks = AppLinks();
 
   @override
   void initState() {
     super.initState();
-    // _sub = linkStream.listen((String? link) {
-    //   if (link != null) {
-    //     pickedfilemodel?.openLocalFile(context);
-    //     print(link);
-    //   }
-    // }, onError: (err) {
-    //   print('Error: $err');
-    // });
+    _initAppLinks();
+  }
+
+  Future<void> _initAppLinks() async {
+    // Check for the initial URI
+    try {
+      final initialUri = await appLinks.getInitialLink();
+      if (initialUri != null) {
+        _handleUri(initialUri);
+      }
+    } catch (e) {
+      print('Error getting initial URI: $e');
+    }
+
+    // Listen for incoming links
+    _uriLinkSubscription = appLinks.uriLinkStream.listen(
+      (Uri? uri) {
+        if (uri != null) {
+          _handleUri(uri);
+        }
+      },
+      onError: (err) {
+        print('Error: $err');
+      },
+    );
+  }
+
+  void _handleUri(Uri uri) {
+    print("***** Received URI: $uri *****");
+    final file = PdfFileModel(
+      fileName: "${uri.toString().length}",
+      filePath: uri.toString(),
+    );
+
+    if (mounted) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => PdfView(pdfFile: file),
+        ),
+      );
+    }
+  }
+
+  @override
+  void dispose() {
+    _uriLinkSubscription?.cancel(); // Cancel the subscription
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(
+      body: Center(child: Text('Listening for PDF links...')),
+    );
   }
 }
--- lib/views/pdf_view.dart
+++ lib/views/pdf_view.dart
@@ -14,22 +14,22 @@ class PdfView extends StatefulWidget {
 }
 
 class _PdfViewState extends State<PdfView> {
-  // final TextEditingController _textEditingController = TextEditingController();
-  // final PdfViewerController _pdfViewerController = PdfViewerController();
+  final TextEditingController _textEditingController = TextEditingController();
+  final PdfViewerController _pdfViewerController = PdfViewerController();
 
-  // @override
-  // void initState() {
-  //   _textEditingController;
-  //   _pdfViewerController;
-  //   super.initState();
-  // }
+  @override
+  void initState() {
+    _textEditingController;
+    _pdfViewerController;
+    super.initState();
+  }
 
-  // @override
-  // void dispose() {
-  //   _textEditingController;
-  //   _pdfViewerController;
-  //   super.dispose();
-  // }
+  @override
+  void dispose() {
+    _textEditingController;
+    _pdfViewerController;
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
